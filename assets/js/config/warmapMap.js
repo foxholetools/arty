18f1709b091015ffa0330c37ee118c1639af8b5d
samuelds <samuelds.pro@gmail.com>
2021-05-15 18:11:13 +0200

    Backup link

--- assets/js/config/warmapMap.js
+++ assets/js/config/warmapMap.js
@@ -1,5 +1,5 @@
-// Maps
-let defaultMap = L.tileLayer('https://assets.foxhole.tools/maps/tiles/warmap/{z}/{z}_{x}_{y}.png');
+// Maps backup (https://foxhole-tools.github.io/assets/public/maps/tiles/warmap/{z}/{z}_{x}_{y}.png)
+let defaultMap = L.tileLayer('https://foxhole-tools.github.io/assets/public/maps/tiles/warmap/{z}/{z}_{x}_{y}.png');
 
 var baseMaps = undefined;
 
